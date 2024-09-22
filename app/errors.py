6413b93031e2d2c814ce00fcc28375ce24b3af25
Alvaro Maceda <alvaro@alvaromaceda.es>
2024-09-22 19:58:41 +0200

    Improves error handling in login

--- app/errors.py
+++ app/errors.py
@@ -6,7 +6,11 @@ def page_not_found(e):
 def forbidden(e):
     return render_template('errors/403.html.j2'), 403
 
+def bad_request(e):
+    return render_template('errors/400.html.j2'), 400
+
 def register_errors(app):
-    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(400, bad_request)
     app.register_error_handler(403, forbidden)
+    app.register_error_handler(404, page_not_found)
 
--- app/routes/lti/login.py
+++ app/routes/lti/login.py
@@ -1,38 +1,65 @@
+from werkzeug.exceptions import Forbidden, BadRequest
+from flask import flash, abort, current_app
+
 from pylti1p3.contrib.flask import (
     FlaskOIDCLogin,
     FlaskRequest,
     FlaskSessionService,
     FlaskCookieService
 )
-from flask import flash, abort, current_app
 
 from . import bp
 from app.lib.lti_lib import lti_tool_conf
 
-@bp.route("/login", methods=["GET", "POST"])
-def login():
-    tool_conf =lti_tool_conf()
-    # launch_data_storage = lti_launch_data_storage()
-    flask_request = FlaskRequest()
-
+def check_target_link_uri(flask_request):
     target_link_uri = flask_request.get_param("target_link_uri")
     if not target_link_uri:
-        raise Exception('Missing "target_link_uri" param')
+        raise BadRequest('Missing "target_link_uri" param')
 
-    oidc_login = FlaskOIDCLogin(
-        request=flask_request,
-        tool_config=tool_conf,
-        session_service=FlaskSessionService(flask_request),
-        cookie_service=FlaskCookieService(flask_request)
-    )
+def check_iss_param(flask_request, tool_conf):
+    iss = flask_request.get_param("iss")
+    if not iss:
+        raise BadRequest('Missing "iss" param in request')
 
     try:
-        login = oidc_login.enable_check_cookies().redirect(target_link_uri)
+        tool_conf.find_registration(iss)
     except Exception as e:
+        message = f'Registration for "iss" ({iss}) not found in tool configuration'
+        current_app.logger.error(f'{message}, exception: {e}')
+        raise Forbidden(message)
+
+@bp.route("/login", methods=["GET", "POST"])
+def login():
+    try:
+        tool_conf =lti_tool_conf()
+        flask_request = FlaskRequest()
+
+        check_target_link_uri(flask_request)
+        check_iss_param(flask_request, tool_conf)
+
+        oidc_login = FlaskOIDCLogin(
+            request=flask_request,
+            tool_config=tool_conf,
+            session_service=FlaskSessionService(flask_request),
+            cookie_service=FlaskCookieService(flask_request)
+        )
+
+        login = oidc_login.enable_check_cookies().redirect(flask_request.get_param("target_link_uri"))
+
+    except BadRequest as e:
+        flash(f'Bad Request: {e.description}', "danger")
+        return abort(400)
+
+    except Forbidden as e:
+        flash(f'{e.description}', "danger")
+        return abort(403)
+
+    except Exception as e:
+        # TO-DO: how to catch the type of error?
         current_app.logger.error(f'Error getting launch: {e}')
         flash(f'Something bad happened in login', "danger")
         # flash(f'The site is not autorized: [{site}]/[{id}]', "danger")
-        abort(403)
+        abort(500)
 
     return login
 
